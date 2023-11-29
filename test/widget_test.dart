d92de66840467dbdac2e06c4c05f59de64a97d78
Tomás L <59000698+XhrdTLS@users.noreply.github.com>
2023-11-29 17:49:47 -0300

    29112023a Add Firebase
    - Se agrega Firebase
    - Se comienza a separar las screens en sus propios archivos

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:classroom_reservation/main.dart';
+import 'package:classroom_reservation/screens/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
